7aed9a8b5c83fa75e8e137386b2ac4a7c0209d05
Sreejith <pssjith@gmail.com>
2025-07-19 20:02:38 +0530

    Made html version of rps

--- scripts.js
+++ scripts.js
@@ -8,10 +8,18 @@ userScore = 0;
 compScore = 0;
 
 console.log("Hello world");
-while(true){
 
-    PlayRound();
-}
+
+var rockBut = document.querySelector("#rock");
+var paperBut = document.querySelector("#paper");
+var scissorsBut = document.querySelector("#scissors");
+rockBut.addEventListener("click",() => GetHumanChoice("rock"));
+paperBut.addEventListener("click",() => GetHumanChoice("paper"));
+scissorsBut.addEventListener("click",() => GetHumanChoice("scissors"));
+
+var winText = document.querySelector("h1");
+var hScore = document.querySelector("#userScore");
+var cScore = document.querySelector("#compScore");
 
 
 
@@ -36,34 +44,48 @@ function GetComputerChoice(){
 
 }
 
-function GetHumanChoice(){
+function GetHumanChoice(hChoice){
 
-    let humanChoice = prompt("Enter Rock, Paper or Scissors");
-
-    return humanChoice.toLowerCase();
-    
-}
-
-function PlayRound(){
-    let hChoice = GetHumanChoice();
     let cChoice = GetComputerChoice();
 
     console.log("Your choice:" + hChoice);
     console.log("Computer choice: " + cChoice);
     if(hChoice === cChoice){
         console.log("Draw");
+        winText.textContent = "Draw";
     }
     else if(winsAgainst[hChoice] === cChoice ){
         console.log("You Win!");
         userScore++;
+        winText.textContent = "You win the round";
 
     }
     else{
         console.log("You lose");
+        winText.textContent = "You lose this round";
         compScore++;
     }
 
+    hScore.textContent = userScore;
+    cScore.textContent = compScore;
+
     console.log("UserScore: " + userScore + " || ComputerScore: " + compScore);
 
+    if(userScore >=5 || compScore >=5){
+        if(userScore >=5){
+
+            winText.textContent = "You win the game";
+        }
+        else{
+            winText.textContent = "The computer wins the game";
+        }
+        rockBut.disabled = true;
+        paperBut.disabled = true;
+        scissorsBut.disabled = true;
+
+    }
+    
 }
+
+
 
